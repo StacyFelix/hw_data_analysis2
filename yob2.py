0d8e8be4dfc1b7c341b70d3184fb58a9e4fcb78c
StacyFelix <felix.gsn.so@gmail.com>
2019-09-13 02:14:37 +0300

    data_analysis2_double

--- yob2.py
+++ yob2.py
@@ -21,7 +21,7 @@ end_year = 2000
 list_names = ['Ruth','Robert']
 
 
-# In[62]:
+# In[152]:
 
 
 # 1.	Построить график изменения количества имен Ruth и Robert с 1900 по 2000.
@@ -36,7 +36,7 @@ res_df = pd.concat(list_df)
 res_df
 
 
-# In[63]:
+# In[131]:
 
 
 res = res_df[(res_df['Name']=='Ruth') | (res_df['Name']=='Robert')].groupby(['Name', 'Year']).sum()
@@ -44,7 +44,7 @@ res = res_df[(res_df['Name']=='Ruth') | (res_df['Name']=='Robert')].groupby(['Na
 res.unstack('Name').plot(kind='area', rot=45, stacked=False)
 
 
-# In[64]:
+# In[132]:
 
 
 # 2.	Построить столбчатую диаграмму по количеству их имен с 1900 по 2000 
@@ -56,28 +56,63 @@ for year in range(begin_year,end_year + 1, 5):
     df = pd.read_csv(full_path, names=['Name', 'Gender', 'Count'], encoding = 'ISO-8859-1')
     df['Year'] = year
     list_df.append(df)
-res_df = pd.concat(list_df)    
-res_df
+res_df2 = pd.concat(list_df)    
+res_df2
 
 
-# In[67]:
+# In[133]:
 
 
-res = res_df[['Year','Count']].groupby(['Year']).count()
+res = res_df2[['Year','Count']].groupby(['Year']).count()
 # res
 res.unstack().plot(kind='bar')
+# plt.barh(res['Name'],res['Count'])
 
 
-# In[113]:
+# In[134]:
 
 
 # 3.	Построить круговую диаграмму по количеству употреблений для ТОП-10 популярных имен, начинающихся на R, за 1950 год.
 
 year = 1950
 full_path = os.path.join(path_dir,f"yob{year}.txt")
-res_df = pd.read_csv(full_path, names=['Name', 'Gender', 'Count'], encoding = 'ISO-8859-1')
-res = res_df[res_df['Name'].str.contains("^R")].groupby(['Name']).sum().sort_values('Count', ascending=0).head(10)
+df = pd.read_csv(full_path, names=['Name', 'Gender', 'Count'], encoding = 'ISO-8859-1')
+df_top10_r = df[df['Name'].str.contains("^R")].groupby(['Name']).sum().sort_values('Count', ascending=0).head(10)
+# df_top10_r
+# df_top10_r.plot(kind='pie', y='Count')
+plt.pie(df_top10_r)
+
+
+# In[221]:
+
+
+# 4.	Построить точечную диаграмму по количеству согласных букв в именах и частоте употребления за 100 лет.
+# Т.е. необходимо сложить всю статистику с 1900 до 2000,
+# сгруппировать по именам, посчитать количество согласных букв в каждом имени
+# и вывести на график отношение "Количество согласных букв в имени" : "Количество употреблений".
+import seaborn as sns
+
+def consonant(str):
+    return len(str) - sum(1 for x in str.lower() if x in 'aeiou')
+
+list_df = []
+for year in range(begin_year,end_year + 1):
+    full_path = os.path.join(path_dir,f"yob{year}.txt")
+    df = pd.read_csv(full_path, names=['Name', 'Gender', 'Count'], encoding = 'ISO-8859-1')
+    list_df.append(df)
+    
+res_df = pd.concat(list_df)    
+res_df['Length'] = res_df['Name'].map(consonant)
+res = res_df.groupby(['Length']).sum()
+# .reset_index(level='Length', drop=True)
+# .sort_values('Count', ascending=0)
 res
+
+
+# In[219]:
+
+
+plt.scatter(res['Count'], res['Length'])
 
 
 # In[ ]:
